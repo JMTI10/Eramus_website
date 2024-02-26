433bdf28eff95dab80e4ff357792ea8a95c74f21
Iuri Gonçalves <48198077+JMTI10@users.noreply.github.com>
2024-02-26 14:34:33 +0200

    Update messages.php

--- messages.php
+++ messages.php
@@ -1,4 +1,9 @@
 <?php  if (count($errors) > 0) : ?>
+  <!--
+ * File:   error.php
+ * Author: Iuri Gonçalves
+ * Date:   2018
+ -->
   <div class="msg">
   	<?php foreach ($errors as $error) : ?>
   	  <span><?php echo $error ?></span>
